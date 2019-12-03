72dc0ec5c9fccb26cf95b67fff6f3104d140f11d
Vladimir Zoubritsky <vladimir@reshuffle.com>
2019-12-03 14:27:30 +0200

    Update interface to authHandler

--- backend/_handler.js
+++ backend/_handler.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import { defaultHandler } from '@reshuffle/server-function';
-import { authRouter } from '@reshuffle/passport';
+import { authHandler } from '@reshuffle/passport';
 
 const app = express();
-app.use(authRouter());
+app.use(authHandler);
 
 // Example of accessing a user
 //
